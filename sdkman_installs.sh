620b5d909dc49a72a57653bcd55c5ce0722ddcfb
Caroline Russell <caroline@appthreat.dev>
2023-12-07 07:48:10 +0000

    Slice update

--- sdkman_installs.sh
+++ sdkman_installs.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 source "/${SDKMAN_DIR}/bin/sdkman-init.sh"
-sdk install java 11.0.21-tem
 sdk install maven 3.10.1
-sdk install java 8.0.392-tem
 sdk install maven 3.11.0
+sdk install java 8.0.392-tem
+sdk install java 11.0.21-tem
