c63584c1e36eff8cd80804373e9e694aca9d9c0b
Caroline Russell <caroline@appthreat.dev>
2023-12-07 22:29:07 -0500

    Generate a shell script for command execution.
    Signed-off-by: Caroline Russell <caroline@appthreat.dev>

--- generate.py
+++ generate.py
@@ -87,54 +87,51 @@ def generate(repo_data, clone_dir, output_dir, slice_types, clone, debug_cmds):
             clone_repo(repo['link'], clone_dir, repo_dir)
 
         if len(repo['pre_build_cmd']) > 0:
-            if not debug_cmds:
-                os.chdir(repo_dir)
+            commands += f'\ncd {repo_dir}'
             cmds = repo['pre_build_cmd'].split(';')
             cmds = [cmd.lstrip().rstrip() for cmd in cmds]
             for cmd in cmds:
                 new_cmd = list(cmd.split(' '))
                 commands += f"\n{subprocess.list2cmdline(new_cmd)}"
-                if not debug_cmds:
-                    subprocess.run(new_cmd, shell=True, encoding='utf-8', check=False)
+                # if not debug_cmds:
+                #     subprocess.run(new_cmd, shell=True, encoding='utf-8', check=False)
 
         if len(repo['build_cmd']) > 0:
             cmds = repo['build_cmd'].split(';')
             cmds = [cmd.lstrip().rstrip() for cmd in cmds]
             for cmd in cmds:
                 new_cmd = list(cmd.split(' '))
-                if debug_cmds:
-                    commands += f"\n{subprocess.list2cmdline(new_cmd)}"
-                else:
-                    subprocess.run(new_cmd, shell=True, encoding='utf-8', check=False)
+                commands += f"\n{subprocess.list2cmdline(new_cmd)}"
+                # else:
+                #     subprocess.run(new_cmd, shell=True, encoding='utf-8', check=False)
 
-        if not debug_cmds:
-            os.chdir(loc)
+        commands += f'\ncd {loc}'
+        if lang == 'java':
+            commands += f'\nsdk use java 20.0.2-tem'
 
         for stype in slice_types:
             slice_file = os.path.join(output_dir, lang, f"{project}-{stype}.json")
             atom_file = os.path.join(repo_dir, f"{project}.atom")
             cmd = ['atom', stype, '-l', lang, '-o', atom_file, '-s', slice_file, repo_dir]
-            if debug_cmds:
-                commands += f"\n{subprocess.list2cmdline(cmd)}"
-            else:
-                subprocess.run(cmd, shell=True, encoding='utf-8', check=False)
+            commands += f"\n{subprocess.list2cmdline(cmd)}"
+            # else:
+            #     subprocess.run(cmd, shell=True, encoding='utf-8', check=False)
 
         commands += '\n\n'
+
+    with open('atom_commands.sh', 'w', encoding='utf-8') as f:
+        f.write('#!/usr/bin/env bash\n')
+        f.write('source "/${SDKMAN_DIR}/bin/sdkman-init.sh"\n')
+        f.write(commands)
 
     if debug_cmds:
         print(commands)
     else:
-        with open('atom_commands.sh', 'w', encoding='utf-8') as f:
-            f.write('#!/usr/bin/env bash\n')
-            f.write('source "/${SDKMAN_DIR}/bin/sdkman-init.sh"\n')
-            f.write(commands)
-
         cp = subprocess.run('atom_commands.sh', shell=True,
             stdout=subprocess.PIPE, stderr=subprocess.STDOUT,
             env=os.environ.copy(), encoding='utf-8', check=False, )
 
-        if cp.returncode != 0:
-            print(cp.stdout)
+        print(cp.stdout)
 
 
 def read_csv(csv_file, langs):
@@ -168,14 +165,13 @@ def run_pre_builds(repo_data, debug_cmds=False):
     cmds = set(cmds)
 
     commands = [c.replace('use', 'install') for c in cmds]
+    with open('sdkman_installs.sh', 'w', encoding='utf-8') as f:
+        f.write('#!/usr/bin/env bash\n')
+        f.write('source "/${SDKMAN_DIR}/bin/sdkman-init.sh"\n')
+        f.write('\n'.join(commands))
     if debug_cmds:
         print('\n'.join(commands))
     else:
-        with open('sdkman_installs.sh', 'w', encoding='utf-8') as f:
-            f.write('#!/usr/bin/env bash\n')
-            f.write('source "/${SDKMAN_DIR}/bin/sdkman-init.sh"\n')
-            f.write('\n'.join(commands))
-
         cp = subprocess.run(
             'sdkman_installs.sh',
             shell=True,
@@ -184,8 +180,7 @@ def run_pre_builds(repo_data, debug_cmds=False):
             env=os.environ.copy(),
             encoding='utf-8', check=False, )
 
-        if cp.returncode != 0:
-            print(cp.stdout)
+        print(cp.stdout)
 
 
 def check_dirs(clone, clone_dir, output_dir):
