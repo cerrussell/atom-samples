7a3e5abb49346830ea79f59b33ca4bb65d3c9a05
Caroline Russell <caroline@appthreat.dev>
2023-12-08 12:50:01 -0500

    Troubleshooting permissions.
    Signed-off-by: Caroline Russell <caroline@appthreat.dev>

--- generate.py
+++ generate.py
@@ -161,7 +161,7 @@ def use_script(file_path, commands, debug_cmds):
     if debug_cmds:
         print(commands)
     else:
-        cmd = ['sudo', file_path]
+        cmd = f'./{file_path}'
         cp = subprocess.run(cmd, shell=True,
             stdout=subprocess.PIPE, stderr=subprocess.STDOUT,
             env=os.environ.copy(), encoding='utf-8', check=False, )
