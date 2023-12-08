6e7b948d162c1639c2f74edc09118f7f7162a196
Caroline Russell <caroline@appthreat.dev>
2023-12-08 12:29:20 -0500

    Run scripts with elevated permissions.
    Signed-off-by: Caroline Russell <caroline@appthreat.dev>

--- generate.py
+++ generate.py
@@ -161,6 +161,7 @@ def use_script(file_path, commands, debug_cmds):
     if debug_cmds:
         print(commands)
     else:
+        cmd = ['sudo', file_path]
         cp = subprocess.run(file_path, shell=True,
             stdout=subprocess.PIPE, stderr=subprocess.STDOUT,
             env=os.environ.copy(), encoding='utf-8', check=False, )
